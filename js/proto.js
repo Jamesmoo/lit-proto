934750910c06ec6d6b7c1d9d61bacf3dbb55681a
Jamesmoo <jamesmoo2090@gmail.com>
2019-04-11 11:27:09 -0600

    fix for codesandbox
    codesandbox

--- js/proto.js
+++ js/proto.js
@@ -1,139 +1,185 @@
-import { html, render } from './lit-html.js'
+import { html, render } from "lit-html";
 
 // import {html, render} from 'https://unpkg.com/lit-html?module';
 //import { repeat } from './repeat.js'
-import { orgs } from './org_data.js'
-import { KeyValDD, uniqueObjBy, compareByProperty } from './at_array_util.js'
+import { orgs } from "./org_data.js";
+import { KeyValDD, uniqueObjBy, compareByProperty } from "./at_array_util.js";
 // import { clearBtnClickHandler } from './events.js'
 // import { clearButton } from './clearButton';
 
-function myfunc() {alert('myfunc nothing');}
+function myfunc() {
+  alert("myfunc nothing");
+}
 
 const dropdowns = {
-	"columnOptions": [
-		{ "value": "Management Assistant", "ddValue": "managementAssistant" },
-		{ "value": "Alternate Management Assistant", "ddValue": "alternateManagementAssistant"}
-	],
-	"operatorOptions": [
-		{"value": "not equal to", "ddValue": "notEqualTo" },
-		{"value": "equal to", "ddValue": "equalTo" },
-		{"value": "contains", "ddValue": "contains" },
-		{"value": "doesn't contain", "ddValue": "doesntContain" }
-	],
-	"primarySupervisorOptions":
-		uniqueObjBy(orgs.map((org) =>  {return new KeyValDD(org.PRIMARY_SUPERVISOR_NAME,org.PRIMARY_SUPERVISOR_ID)}).sort(compareByProperty('value')),x => x.ddValue)
-	,
-	"altSupervisorOptions":
-		uniqueObjBy(orgs.map((org) =>  {return new KeyValDD(org.ALTERNATE_SUPERVISOR_NAME,org.ALTERNATE_SUPERVISOR_ID)}).sort(compareByProperty('value')),x => x.ddValue)
-	,
-	"timeKeeperOptions": /*[
+  columnOptions: [
+    { value: "Management Assistant", ddValue: "managementAssistant" },
+    {
+      value: "Alternate Management Assistant",
+      ddValue: "alternateManagementAssistant"
+    }
+  ],
+  operatorOptions: [
+    { value: "not equal to", ddValue: "notEqualTo" },
+    { value: "equal to", ddValue: "equalTo" },
+    { value: "contains", ddValue: "contains" },
+    { value: "doesn't contain", ddValue: "doesntContain" }
+  ],
+  primarySupervisorOptions: uniqueObjBy(
+    orgs
+      .map(org => {
+        return new KeyValDD(
+          org.PRIMARY_SUPERVISOR_NAME,
+          org.PRIMARY_SUPERVISOR_ID
+        );
+      })
+      .sort(compareByProperty("value")),
+    x => x.ddValue
+  ),
+  altSupervisorOptions: uniqueObjBy(
+    orgs
+      .map(org => {
+        return new KeyValDD(
+          org.ALTERNATE_SUPERVISOR_NAME,
+          org.ALTERNATE_SUPERVISOR_ID
+        );
+      })
+      .sort(compareByProperty("value")),
+    x => x.ddValue
+  ),
+  /*[
 		{"value": "Smith, Morty (309)", "ddValue": "smithMorty" },
 		{"value": "Steiner, Braden (309)", "ddValue": "steinerBraden" },
 		{"value": "Adams, James (309)", "ddValue": "adamsJames" }]*/
-		uniqueObjBy(orgs.map((org) =>  {return new KeyValDD(org.PRIMARY_TIMEKEEPER_NAME,org.PRIMARY_TIMEKEEPER_ID)}).sort(compareByProperty('value')),x => x.ddValue)
-	,
-	"altTimeKeeperOptions":
-	  uniqueObjBy(orgs.map((org) =>  {return new KeyValDD(org.ALTERNATE_TIMEKEEPER_NAME,org.ALTERNATE_TIMEKEEPER_ID)}).sort(compareByProperty('value')),x => x.ddValue)
-	,
-	"logicalOptions": [
-		{"value": "And", "ddValue": "and" },
-		{"value": "Or", "ddValue": "or" }
-	]
+  timeKeeperOptions: uniqueObjBy(
+    orgs
+      .map(org => {
+        return new KeyValDD(
+          org.PRIMARY_TIMEKEEPER_NAME,
+          org.PRIMARY_TIMEKEEPER_ID
+        );
+      })
+      .sort(compareByProperty("value")),
+    x => x.ddValue
+  ),
+  altTimeKeeperOptions: uniqueObjBy(
+    orgs
+      .map(org => {
+        return new KeyValDD(
+          org.ALTERNATE_TIMEKEEPER_NAME,
+          org.ALTERNATE_TIMEKEEPER_ID
+        );
+      })
+      .sort(compareByProperty("value")),
+    x => x.ddValue
+  ),
+  logicalOptions: [
+    { value: "And", ddValue: "and" },
+    { value: "Or", ddValue: "or" }
+  ]
 };
 
-const dropdownOptions = (options) => html`
-	${options.map( (opt) =>
-        html`<option value="${opt.ddValue}">${opt.value}</option>` )}
+const dropdownOptions = options => html`
+  ${options.map(
+    opt =>
+      html`
+        <option value="${opt.ddValue}">${opt.value}</option>
+      `
+  )}
 `;
-    
-const searchBox = (dropdowns) => html`
-	
-  Search Criteria:<br>
+
+const searchBox = dropdowns => html`
+  Search Criteria:<br />
   <label class="input-label">
-    <input class="input-checkbox" type="checkbox">
-  </label> Search active organizations only
-​
+    <input class="input-checkbox" type="checkbox" />
+  </label>
+  Search active organizations only ​
   <div class="input-parameters">
     <select class="control-management">
-			${dropdownOptions(dropdowns.columnOptions)}
+      ${dropdownOptions(dropdowns.columnOptions)}
     </select>
     <select class="control-options">
-			${dropdownOptions(dropdowns.operatorOptions)}
+      ${dropdownOptions(dropdowns.operatorOptions)}
     </select>
     <select class="control-employee">
-			${dropdownOptions(dropdowns.timeKeeperOptions)}
+      ${dropdownOptions(dropdowns.timeKeeperOptions)}
     </select>
     <select class="control-assistant">
-			${dropdownOptions(dropdowns.logicalOptions)}
+      ${dropdownOptions(dropdowns.logicalOptions)}
     </select>
   </div>
-​
-  <button @click=${e => console.log('clicked')}>Click Me</button>
+  ​
+  <button @click=${e => console.log("clicked")}>Click Me</button>
   <button class="button search">Search</button>
-`
-render(searchBox(dropdowns), document.getElementById('search-container'));
+`;
+render(searchBox(dropdowns), document.getElementById("search-container"));
 
-const searchedOrgs = (orgs) => html`
-    
-    Searched Organizations:[hard coded]
-    <table>
+const searchedOrgs = orgs => html`
+  Searched Organizations:[hard coded]
+  <table>
+    <tr>
+      <th>ID</th>
+      <th>Official Name</th>
+      <th>Short Name</th>
+      <th>Type</th>
+      <th>Parent Organization</th>
+      <th>Description</th>
+      <th>Primary Supervisor</th>
+      <th>Alternate Supervisor</th>
+      <th>Primary Management Assistant</th>
+      <th>Alternate management Assistant</th>
+      <th>Active</th>
+    </tr>
+    ${orgs.map(
+      org => html`
         <tr>
-            <th>ID</th>
-            <th>Official Name</th>
-            <th>Short Name</th>
-            <th>Type</th>
-            <th>Parent Organization</th>
-            <th>Description</th>
-            <th>Primary Supervisor</th>
-            <th>Alternate Supervisor</th>
-            <th>Primary Management Assistant</th>
-            <th>Alternate management Assistant</th>
-            <th>Active</th>
+          <td>${org.ORGANIZATION_ID}</td>
+          <td>${org.OFFICIAL_NAME}</td>
+          <td>${org.ORGANIZATION_NAME}</td>
+          <td>${org.ORGANIZATION_TYPE_CD}</td>
+          <td>${org.NEXT_HIGHER_ORG_ID}</td>
+          <td>${org.DESCRIPTION}</td>
+          <td>${org.PRIMARY_SUPERVISOR_NAME}</td>
+          <td>${org.ALTERNATE_SUPERVISOR_NAME}</td>
+          <td>${org.PRIMARY_TIMEKEEPER_NAME}</td>
+          <td>${org.ALTERNATE_TIMEKEEPER_NAME}</td>
+          <td>${org.ACTIVE_FLAG}</td>
         </tr>
-        ${ orgs.map( (org) => html`
-        <tr>
-            <td>${org.ORGANIZATION_ID}</td>
-            <td>${org.OFFICIAL_NAME}</td>
-            <td>${org.ORGANIZATION_NAME}</td>
-            <td>${org.ORGANIZATION_TYPE_CD}</td>
-            <td>${org.NEXT_HIGHER_ORG_ID}</td>
-            <td>${org.DESCRIPTION}</td>
-            <td>${org.PRIMARY_SUPERVISOR_NAME}</td>
-            <td>${org.ALTERNATE_SUPERVISOR_NAME}</td>
-            <td>${org.PRIMARY_TIMEKEEPER_NAME}</td>
-            <td>${org.ALTERNATE_TIMEKEEPER_NAME}</td>
-            <td>${org.ACTIVE_FLAG}</td>
-        </tr>
-        `)}
-    </table>
+      `
+    )}
+  </table>
 `;
 
-render(searchedOrgs(orgs), document.getElementById('results-container'));
-
+render(searchedOrgs(orgs), document.getElementById("results-container"));
 
 const list = [
-    {
-        id: '100',
-        officialName: '309 SMXG/MXDS',
-        shortName: '309 SMXG/MXDS',
-        type: 'Squadron',
-        parentOrg: '309 SMXG',
-        description: 'Business Operations',
-        primarySupervisor: 'Brown, Doc',
-        alternateSupervisor: '',
-        pma: 'Simpsonss, Homer',
-        ama: 'Griffin, Peter',
-        active: 'Y'
-    },
-]
+  {
+    id: "100",
+    officialName: "309 SMXG/MXDS",
+    shortName: "309 SMXG/MXDS",
+    type: "Squadron",
+    parentOrg: "309 SMXG",
+    description: "Business Operations",
+    primarySupervisor: "Brown, Doc",
+    alternateSupervisor: "",
+    pma: "Simpsonss, Homer",
+    ama: "Griffin, Peter",
+    active: "Y"
+  }
+];
 
-const searchedOrgsList = (org) => html`
-    ${repeat(org, (i) => i.id, )}
+const searchedOrgsList = org => html`
+  ${repeat(org, i => i.id)}
 `;
 
-const listTemplate = (items) => html`
+const listTemplate = items => html`
   <ul>
-    ${repeat(items, (i) => i.id, (i, index) => html`
-      <li>${index}: ${i.name}</li>`)}
+    ${repeat(
+      items,
+      i => i.id,
+      (i, index) => html`
+        <li>${index}: ${i.name}</li>
+      `
+    )}
   </ul>
 `;
